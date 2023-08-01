d8d9eaf7279c76dbef0deea74e3fa5a89dd1dd8a
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-08-01 11:32:35 +0200

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Projet01-Ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

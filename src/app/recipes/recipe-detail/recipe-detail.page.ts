cfa2ff136bbd335045e695458297df32ccaf861a
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-08-02 12:06:07 +0200

    recipe-item

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -16,29 +16,49 @@ export class RecipeDetailPage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router) { }
+    private router: Router,
+    private alertCtr: AlertController) { }
 
-  ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      if(!paramMap.has('recipeId')) return;
-      const recipeId = paramMap.get('recipeId');
-      this.loadedRecipe = this.recipesService.getRecipe(recipeId!);
-    })
-  }
+    ngOnInit() {
+      this.activatedRoute.paramMap.subscribe(paramMap => {
+        if(!paramMap.has('recipeId')) return;
+        const recipeId = paramMap.get('recipeId');
+        this.loadedRecipe = this.recipesService.getRecipe(recipeId!);
+      })
+    }
 
-  onDeleteRecipe() {
-    // this.alertCtr.create({
-    //   header:'Are you sur?',
-    //   message:'Do you really want to delete the recipe? '
-    //   buttons:
-    // })
-    this.recipesService.deleteRecipe(this.loadedRecipe?.id!);
-    this.router.navigate(['/recipes']);
-  }
+//   onDeleteRecipe() {
+//     // this.alertCtr.create({
+//     //   header:'Are you sur?',
+//     //   message:'Do you really want to delete the recipe? '
+//     //   buttons:
+//     // })
+//     this.recipesService.deleteRecipe(this.loadedRecipe?.id!);
+//     this.router.navigate(['/recipes']);
+//   }
 
+// }
+
+
+onDeleteRecipe() {
+  this.alertCtr.create({
+    header: 'Are you sur ?',
+    message: 'Do you really want to delete the recipe ?',
+    buttons: [{
+      text: 'Cancel',
+      role: 'cancel'
+    },{
+      text: 'Delete',
+      handler: () => {
+        this.recipesService.deleteRecipe(this.loadedRecipe?.id!);
+        this.router.navigate(['/recipes']);
+      }
+    }]
+  }).then(alertEl => {
+    alertEl.present();
+  })
 }
 
-
-
+}
 
 
